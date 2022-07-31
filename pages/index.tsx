67e7e6643802fbc70eb366bd9dfc71ad46221024
dimusiq <dumojke@gmail.com>
2022-07-31 19:46:45 +0500

    configuring project and simple components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Htag } from '../components/index';
+import { Button, Htag, P, Tag } from '../components/index';
 
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag='h1'>Текст</Htag>
+			<Button appearance='primary' arrow='right'>Button</Button>
+			<Button appearance='ghost' arrow='down'>Button</Button>
+			<P size='l'>Большой</P>
+			<P size='m'>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s' color='ghost'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag size='s' color='primary'>Green</Tag>
+
 		</>
 	);
 }
