a5a5cde609f6409f1a47c21138e90b594a569f0a
dimusiq <dumojke@gmail.com>
2022-08-01 23:57:08 +0500

    rating component improving

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components/index';
 
-
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
 	return (
 		<>
 			<Htag tag='h1'>Текст</Htag>
-			<Button appearance='primary' arrow='right'>Button</Button>
-			<Button appearance='ghost' arrow='down'>Button</Button>
+			<Button appearance='primary' arrow='right'>
+				Button
+			</Button>
+			<Button appearance='ghost' arrow='down'>
+				Button
+			</Button>
 			<P size='l'>Большой</P>
 			<P size='m'>Средний</P>
 			<P size='s'>Маленький</P>
-			<Tag size='s' color='ghost'>Ghost</Tag>
-			<Tag size='m' color='red'>Red</Tag>
-			<Tag size='s' color='green'>Green</Tag>
-			<Tag size='s' color='primary'>Green</Tag>
-			<Rating rating={4}/>
+			<Tag size='s' color='ghost'>
+				Ghost
+			</Tag>
+			<Tag size='m' color='red'>
+				Red
+			</Tag>
+			<Tag size='s' color='green'>
+				Green
+			</Tag>
+			<Tag size='s' color='primary'>
+				Green
+			</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	);
 }
