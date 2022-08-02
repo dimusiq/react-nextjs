9196c433767f3b8fd6194728728ead1f3fb02612
dimusiq <dumojke@gmail.com>
2022-08-02 20:01:42 +0500

    added HOC component

--- components/P/P.tsx
+++ components/P/P.tsx
@@ -2,17 +2,21 @@ import { Pprops } from './P.props';
 import styles from './P.module.css';
 import cn from 'classnames';
 
-export const P = ({ children, size ='m', className, ...props
+export const P = ({
+	children,
+	size = 'm',
+	className,
+	...props
 }: Pprops): JSX.Element => {
-  return (
-    <p 
-    className={cn(styles.p, className, {
-      [styles.s]: size == 's',
-      [styles.m]: size == 'm',
-      [styles.l]: size == 'l',
-    })}
-    {...props}
-    >{children}</p>
-  )
- 
-}
+	return (
+		<p
+			className={cn(styles.p, className, {
+				[styles.s]: size == 's',
+				[styles.m]: size == 'm',
+				[styles.l]: size == 'l',
+			})}
+			{...props}>
+			{children}
+		</p>
+	);
+};
