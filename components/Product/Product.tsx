1a7ba794a4226fd59c2ed860ee1f70b85da19866
dimusiq <dumojke@gmail.com>
2022-08-29 21:10:14 +0500

    added word declension function

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -5,7 +5,7 @@ import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
-import { priceRu } from '../../helpers/helpers';
+import { declOfNum, priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
 
 export const Product = ({
@@ -45,26 +45,38 @@ export const Product = ({
 			</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.rateTitle}>
+				{product.reviewCount}{' '}
+				{declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+			</div>
 			<Divider className={styles.hr} />
 
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
+			<div className={styles.feature}>
+				{product.characteristics.map((c) => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDots}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advantage}>
 				<div className={styles.advBlock}>
-					{product.advantages && <div className={styles.advantages}>
-						<div className={styles.advTitle}>Преимущества</div>
-						<div>{product.advantages}</div>
-					</div>}
-				</div> 
+					{product.advantages && (
+						<div className={styles.advantages}>
+							<div className={styles.advTitle}>Преимущества</div>
+							<div>{product.advantages}</div>
+						</div>
+					)}
+				</div>
 				<div>
-					{product.disadvantages && <div className={styles.disadvantages}>
-						
-						<div className={styles.advTitle}>Недостатки</div>
-					<div>{product.disadvantages} </div>
-
-					</div>}
-					
+					{product.disadvantages && (
+						<div className={styles.disadvantages}>
+							<div className={styles.advTitle}>Недостатки</div>
+							<div>{product.disadvantages} </div>
+						</div>
+					)}
 				</div>
 			</div>
 			<Divider className={styles.hr} />
