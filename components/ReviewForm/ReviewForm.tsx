dbc5938961927f528a702d8db45aae0484f6c97c
dimusiq <dumojke@gmail.com>
2022-09-07 21:27:54 +0500

    added styles to ReviewFrom component

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,5 +1,6 @@
 import { ReviewFormProps } from './ReviewForm.props';
 import styles from './ReviewForm.module.css';
+import CloseIcon from './close.svg';
 import cn from 'classnames';
 import { Input } from '../Input/Input';
 import { Rating } from '../Rating/Rating';
@@ -12,18 +13,30 @@ export const ReviewForm = ({
 	...props
 }: ReviewFormProps): JSX.Element => {
 	return (
-		<div className={cn(styles.reviewForm, className)} {...props}>
-			<Input />
-			<Input />
-			<div className={styles.rating}>
-				<span>Оценка:</span>
-				<Rating rating={0}/>
+		<>
+			<div className={cn(styles.reviewForm, className)} {...props}>
+				<Input placeholder='Имя' />
+				<Input placeholder='Заголовок отзыва' className={styles.title} />
+				<div className={styles.rating}>
+					<span>Оценка:</span>
+					<Rating rating={0} />
+				</div>
+				<Textarea placeholder='Текст отзыва' className={styles.description} />
+				<div className={styles.submit}>
+					<Button appearance='primary'>Отправить</Button>
+					<span className={styles.info}>
+						* Перед публикацией отзыв пройдет предварительную модерацию и
+						проверку
+					</span>
+				</div>
 			</div>
-			<Textarea/>
-			<div className={styles.submit}>
-				<Button appearance='primary'>Отправить</Button>
-				<span>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
+			<div className={styles.success}>
+				<div className={styles.successTitle}>Ваш отзыв отправлен</div>
+				<div>
+					Спасибо, ваш отзыв будет опубликован после проверки.
+				</div>
+				<CloseIcon className={styles.close}/>
 			</div>
-		</div>
+		</>
 	);
 };
