4196e40360e7e9a4c019e154c76e27b2281b4b44
dimusiq <dumojke@gmail.com>
2022-10-01 12:22:14 +0500

    added button animation

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,7 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import ArrowIcon from './arrow.svg';
 import cn from 'classnames';
+import { motion } from 'framer-motion';
 
 export const Button = ({
 	appearance,
@@ -10,8 +11,11 @@ export const Button = ({
 	className,
 	...props
 }: ButtonProps): JSX.Element => {
+	
+
 	return (
-		<button
+		<motion.button
+			whileHover={{ scale: 1.05}}
 			className={cn(styles.button, className, {
 				[styles.primary]: appearance == 'primary',
 				[styles.ghost]: appearance == 'ghost',
@@ -26,6 +30,6 @@ export const Button = ({
 					<ArrowIcon />
 				</span>
 			)}
-		</button>
+		</motion.button>
 	);
 };
